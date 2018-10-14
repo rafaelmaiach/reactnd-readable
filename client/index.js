875ae15f88c09f87d91deb028d387998fe111b08
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-10-14 18:06:25 -0300

    feat: Initialize with redux on app

--- client/index.js
+++ client/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import middleware from './middleware';
 
 import Application from './Application';
 
-render(<Application />, document.getElementById('root'));
+const store = createStore(reducer, middleware);
+
+render(
+  <Provider store={store}>
+    <Application />
+  </Provider>, document.getElementById('root')
+);
