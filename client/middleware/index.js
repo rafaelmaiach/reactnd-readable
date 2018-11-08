ca15731a4778b43c310ab15838718588e6d039a1
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-08 14:39:22 -0200

    refactor: change export default middlewares

--- client/middleware/index.js
+++ client/middleware/index.js
@@ -2,7 +2,6 @@ import thunk from 'redux-thunk';
 import { applyMiddleware } from 'redux';
 import logger from './logger';
 
-export default applyMiddleware(
-  thunk,
-  logger
-);
+const middlewares = applyMiddleware(thunk, logger);
+
+export default middlewares;
