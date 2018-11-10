601f5e6e9729ef9d7543d14e123fdc7233a419cf
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-10 11:13:32 -0200

    docs: comment FormField and add proptypes

--- client/components/form/FormField.js
+++ client/components/form/FormField.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Form } from 'antd';
 
+/**
+ * @constructor FormField
+ * @param {object} {label, component} - Label is the value to show on top of field, component is the react component to be rendered
+ */
 const FormField = ({ label, component }) => (
   <div key={label} className="column is-10">
     <span className="title is-size-6">{label}</span>
@@ -9,5 +14,10 @@ const FormField = ({ label, component }) => (
     </Form.Item>
   </div>
 );
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  component: PropTypes.node.isRequired,
+};
 
 export default FormField;
