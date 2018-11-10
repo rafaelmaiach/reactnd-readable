6a4cfc3709060014bcc119195d31e251b09af404
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-10 14:15:58 -0200

    refactor: convert form to functional component

--- client/components/form/Form.js
+++ client/components/form/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -6,65 +6,28 @@ import { Form, Button } from 'antd';
 
 import { getCreateField } from 'Utils/form.helpers';
 
-class PostForm extends Component {
-  static propTypes = {
-    categories: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string,
-      path: PropTypes.string,
-    })).isRequired,
-    form: PropTypes.shape({
-      validateFields: PropTypes.func,
-      getFieldDecorator: PropTypes.func,
-      setFieldsValue: PropTypes.func,
-    }).isRequired,
-    postInfo: PropTypes.shape({
-      title: PropTypes.string,
-      message: PropTypes.string,
-    }),
-    isEdition: PropTypes.bool,
-    createPost: PropTypes.func.isRequired,
-    updatePost: PropTypes.func.isRequired,
-    cancelEdition: PropTypes.func,
-  }
-
-  static defaultProps = {
-    isEdition: false,
-    postInfo: null,
-    cancelEdition: null,
-  }
-
-  constructor(props) {
-    super(props);
-
-    const {
-      form: {
-        validateFields, getFieldDecorator, setFieldsValue,
-      },
-      isEdition,
-      postInfo,
-    } = props;
-
-    // Add form helper functions to class to easy access
-    this.getFieldDecorator = getFieldDecorator;
-    this.validateFields = validateFields;
-    this.setFieldsValue = setFieldsValue;
-    this.isEdition = isEdition;
-    this.postInfo = postInfo;
-  }
+const PostForm = (props) => {
+  const {
+    form: { validateFields, getFieldDecorator },
+    isEdition,
+    postInfo,
+    cancelEdition,
+    createPost,
+    updatePost,
+    categories,
+  } = props;
 
   /**
    * @method PostForm#handleSubmit
    * @description Submit the form information to create or edit a post
    */
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    const { createPost, updatePost } = this.props;
 
     // Validate the form using a built-in form validation from ant design form
     // Call update or create post dispatch based on is edition or new post only if doesn't happened any error
-    this.validateFields((err, data) => {
-      if (!err && this.isEdition) {
+    validateFields((err, data) => {
+      if (!err && isEdition) {
         updatePost(data);
         return;
       }
@@ -73,26 +36,24 @@ class PostForm extends Component {
         createPost(data);
       }
     });
-  }
+  };
 
   /**
    * @method PostForm#createFields
    * @description Create only the needed fields for the form (new or edition).
    */
-  createFields = () => {
-    const { postInfo } = this.props;
-    const createField = getCreateField(this.getFieldDecorator, postInfo);
+  const createFields = () => {
+    const createField = getCreateField(getFieldDecorator, postInfo);
 
     let fieldsData = null;
 
     // Edition form has less fields than new post form
-    if (this.isEdition) {
+    if (isEdition) {
       fieldsData = [
         { id: 'title', label: 'Title', type: 'input' },
         { id: 'message', label: 'Message', type: 'textarea' },
       ];
     } else {
-      const { categories } = this.props;
       fieldsData = [
         { id: 'title', label: 'Title', type: 'input' },
         { id: 'author', label: 'Author', type: 'input' },
@@ -104,36 +65,59 @@ class PostForm extends Component {
     }
 
     return fieldsData.map(createField);
-  }
+  };
 
-  render() {
-    const { cancelEdition } = this.props;
-    const fields = this.createFields();
+  const fields = createFields();
 
-    return (
-      <Form onSubmit={this.handleSubmit} className="columns is-multiline is-centered">
-        {fields}
-        <div className="column is-10 form-buttons-container">
-          <div className="columns">
-            <div className="column">
-              <Button className="button is-primary form-submit-button" type="primary" htmlType="submit">
-                {this.isEdition ? 'Save' : 'Create'}
-              </Button>
-            </div>
-            {
-              this.isEdition && (
-                <div className="column">
-                  <Button className="button is-danger form-submit-button" type="primary" onClick={cancelEdition}>
-                    Cancel
-                  </Button>
-                </div>)
-            }
+  return (
+    <Form onSubmit={handleSubmit} className="columns is-multiline is-centered">
+      {fields}
+      <div className="column is-10 form-buttons-container">
+        <div className="columns">
+          <div className="column">
+            <Button className="button is-primary form-submit-button" type="primary" htmlType="submit">
+              {isEdition ? 'Save' : 'Create'}
+            </Button>
           </div>
+          {
+            isEdition && (
+              <div className="column">
+                <Button className="button is-danger form-submit-button" type="primary" onClick={cancelEdition}>
+                    Cancel
+                </Button>
+              </div>)
+          }
         </div>
-      </Form>
-    );
-  }
-}
+      </div>
+    </Form>
+  );
+};
+
+PostForm.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    path: PropTypes.string,
+  })).isRequired,
+  form: PropTypes.shape({
+    validateFields: PropTypes.func,
+    getFieldDecorator: PropTypes.func,
+    setFieldsValue: PropTypes.func,
+  }).isRequired,
+  postInfo: PropTypes.shape({
+    title: PropTypes.string,
+    message: PropTypes.string,
+  }),
+  isEdition: PropTypes.bool,
+  createPost: PropTypes.func.isRequired,
+  updatePost: PropTypes.func.isRequired,
+  cancelEdition: PropTypes.func,
+};
+
+PostForm.defaultProps = {
+  isEdition: false,
+  postInfo: null,
+  cancelEdition: null,
+};
 
 const mapStateToProps = ({ categories }) => ({
   categories: Object.values(categories),
