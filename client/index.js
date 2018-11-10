c43543d96a5b0b2b22a0e1cd0d7d7ef595cb7320
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-10 16:04:40 -0200

    chore: update react to enable hooks

--- client/index.js
+++ client/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { setConfig } from 'react-hot-loader';
 import reducer from './reducers';
 import middleware from './middleware';
 
 import Application from './Application';
 
 const store = createStore(reducer, middleware);
+
+setConfig({ pureSFC: true });
 
 render(
   <Provider store={store}>
