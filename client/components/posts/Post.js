f71d1b299c73fdf1b13d7e7b1c515e035e5bba6c
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-11 11:32:16 -0200

    chore: rollback react to 16.6.1 and remove hooks as it's bugged with react hot loader

--- client/components/posts/Post.js
+++ client/components/posts/Post.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -10,32 +10,30 @@ import { handleDeletePost } from 'Actions/posts';
 import NewPost from './NewPost';
 import Vote from './Vote';
 
-
-const Post = (props) => {
-  const [isEdition, toggleEdition] = useState(false);
-
-  const {
-    id,
-    category,
-    title,
-    author,
-    body,
-    timestamp,
-    commentCount,
-    voteScore,
-    deletePost,
-  } = props;
-
-  const postInfo = {
-    id,
-    title,
-    message: body,
+class Post extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    timestamp: PropTypes.number.isRequired,
+    commentCount: PropTypes.number.isRequired,
+    voteScore: PropTypes.number.isRequired,
+    deletePost: PropTypes.func.isRequired,
   };
 
-  const togglePostEdition = () => toggleEdition(!isEdition);
+  state = {
+    isEdition: false,
+  }
 
-  const onConfirm = () => {
+  toggleEdition = () => this.setState(({ isEdition }) => ({ isEdition: !isEdition }));
+
+  onConfirm = () => {
+    const { id, title, deletePost } = this.props;
+
     deletePost(id);
+
     message.success(
       <span className="notification is-success">
         {`${title} delete successful!`}
@@ -43,60 +41,71 @@ const Post = (props) => {
     );
   };
 
-  const onCancel = () => message.error(
-    <span className="notification is-danger">
-      {`${title} delete canceled!`}
-    </span>
-  );
+  onCancel = () => {
+    const { title } = this.props;
+    message.error(
+      <span className="notification is-danger">
+        {`${title} delete canceled!`}
+      </span>
+    );
+  };
 
-  return (
-    isEdition
-      ? <NewPost isEdition={isEdition} cancelEdition={togglePostEdition} postInfo={postInfo} />
-      : (
-        <div className="column is-6">
-          <div className="card">
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content">
-                  <Link to={`/${category}/${id}`}>
-                    <p className="title is-4">{title}</p>
-                  </Link>
-                  <p className="subtitle is-6">{author}</p>
+  render() {
+    const { isEdition } = this.state;
+    const {
+      id,
+      category,
+      title,
+      author,
+      body,
+      timestamp,
+      commentCount,
+      voteScore,
+    } = this.props;
+
+    const postInfo = {
+      id,
+      title,
+      message: body,
+    };
+
+    return (
+      isEdition
+        ? <NewPost isEdition={isEdition} cancelEdition={this.toggleEdition} postInfo={postInfo} />
+        : (
+          <div className="column is-6">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <Link to={`/${category}/${id}`}>
+                      <p className="title is-4">{title}</p>
+                    </Link>
+                    <p className="subtitle is-6">{author}</p>
+                  </div>
+                </div>
+
+                <div className="content">
+                  {body}
+                  <br />
+                  <br />
+                  <p>{`Time: ${timestamp}`}</p>
+                  <p>{`Comments: ${commentCount}`}</p>
+                  <Vote postId={id} score={voteScore} />
                 </div>
               </div>
-
-              <div className="content">
-                {body}
-                <br />
-                <br />
-                <p>{`Time: ${timestamp}`}</p>
-                <p>{`Comments: ${commentCount}`}</p>
-                <Vote postId={id} score={voteScore} />
-              </div>
+              <footer className="card-footer">
+                <a role="button" className="card-footer-item" onClick={this.toggleEdition}>Edit</a>
+                <Popconfirm title="Are you sure delete this post?" onConfirm={this.onConfirm} onCancel={this.onCancel} okText="Yes" cancelText="No">
+                  <a role="button" className="card-footer-item">Delete</a>
+                </Popconfirm>
+              </footer>
             </div>
-            <footer className="card-footer">
-              <a role="button" className="card-footer-item" onClick={togglePostEdition}>Edit</a>
-              <Popconfirm title="Are you sure delete this post?" onConfirm={onConfirm} onCancel={onCancel} okText="Yes" cancelText="No">
-                <a role="button" className="card-footer-item">Delete</a>
-              </Popconfirm>
-            </footer>
           </div>
-        </div>
-      )
-  );
-};
-
-Post.propTypes = {
-  id: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
-  timestamp: PropTypes.number.isRequired,
-  commentCount: PropTypes.number.isRequired,
-  voteScore: PropTypes.number.isRequired,
-  deletePost: PropTypes.func.isRequired,
-};
+        )
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   deletePost: (postId) => {
