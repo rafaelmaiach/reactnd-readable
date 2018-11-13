9e2f818339d68018194c7f5397e227d346669f6d
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-13 21:11:52 -0200

    fix: fix small lints on api server

--- api-server/categories.js
+++ api-server/categories.js
@@ -1,41 +1,40 @@
-const clone = require('clone')
-const config = require('./config')
+const clone = require('clone');
 
-let db = {}
+const db = {};
 
 const defaultData = {
   categories: [
-      {
-        name: 'react',
-        path: 'react'
-      },
-      {
-        name: 'redux',
-        path: 'redux'
-      },
-      {
-        name: 'udacity',
-        path: 'udacity'
-      }
-  ]
-}
+    {
+      name: 'react',
+      path: 'react',
+    },
+    {
+      name: 'redux',
+      path: 'redux',
+    },
+    {
+      name: 'udacity',
+      path: 'udacity',
+    },
+  ],
+};
 
-function getData (token) {
-  //Each token has it's own copy of the DB. The token in this case is like an app id.
-  let data = db[token]
-  //This populates the default user data if there isn't any in the db.
+function getData(token) {
+  // Each token has it's own copy of the DB. The token in this case is like an app id.
+  let data = db[token];
+  // This populates the default user data if there isn't any in the db.
   if (data == null) {
-    data = db[token] = clone(defaultData)
+    data = db[token] = clone(defaultData);
   }
-  return data
+  return data;
 }
 
-function getAll (token) {
+function getAll(token) {
   return new Promise((res) => {
-    res(getData(token))
-  })
+    res(getData(token));
+  });
 }
 
 module.exports = {
-  getAll
-}
+  getAll,
+};
--- api-server/posts.js
+++ api-server/posts.js
@@ -1,9 +1,9 @@
-const clone = require('clone')
+const clone = require('clone');
 
-let db = {}
+const db = {};
 
 const defaultData = {
-  "8xf0y6ziyjabvozdd253nd": {
+  '8xf0y6ziyjabvozdd253nd': {
     id: '8xf0y6ziyjabvozdd253nd',
     timestamp: 1467166872634,
     title: 'Udacity is the best place to learn React',
@@ -12,9 +12,9 @@ const defaultData = {
     category: 'react',
     voteScore: 6,
     deleted: false,
-    commentCount: 2
+    commentCount: 2,
   },
-  "6ni6ok3ym7mf1p33lnez": {
+  '6ni6ok3ym7mf1p33lnez': {
     id: '6ni6ok3ym7mf1p33lnez',
     timestamp: 1468479767190,
     title: 'Learn Redux in 10 minutes!',
@@ -23,50 +23,50 @@ const defaultData = {
     category: 'redux',
     voteScore: -5,
     deleted: false,
-    commentCount: 0
-  }
-}
+    commentCount: 0,
+  },
+};
 
-function getData (token) {
-  let data = db[token]
+function getData(token) {
+  let data = db[token];
   if (data == null) {
-    data = db[token] = clone(defaultData)
+    data = db[token] = clone(defaultData);
   }
-  return data
+  return data;
 }
 
-function getByCategory (token, category) {
+function getByCategory(token, category) {
   return new Promise((res) => {
-    let posts = getData(token)
-    let keys = Object.keys(posts)
-    let filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted)
-    res(filtered_keys.map(key => posts[key]))
-  })
+    const posts = getData(token);
+    const keys = Object.keys(posts);
+    const filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted);
+    res(filtered_keys.map(key => posts[key]));
+  });
 }
 
-function get (token, id) {
+function get(token, id) {
   return new Promise((res) => {
-    const posts = getData(token)
+    const posts = getData(token);
     res(
       posts[id].deleted
         ? {}
         : posts[id]
-    )
-  })
+    );
+  });
 }
 
-function getAll (token) {
+function getAll(token) {
   return new Promise((res) => {
-    const posts = getData(token)
-    let keys = Object.keys(posts)
-    let filtered_keys = keys.filter(key => !posts[key].deleted)
-    res(filtered_keys.map(key => posts[key]))
-  })
+    const posts = getData(token);
+    const keys = Object.keys(posts);
+    const filtered_keys = keys.filter(key => !posts[key].deleted);
+    res(filtered_keys.map(key => posts[key]));
+  });
 }
 
-function add (token, post) {
+function add(token, post) {
   return new Promise((res) => {
-    let posts = getData(token)
+    const posts = getData(token);
 
     posts[post.id] = {
       id: post.id,
@@ -77,53 +77,53 @@ function add (token, post) {
       category: post.category,
       voteScore: 1,
       deleted: false,
-      commentCount: 0
-    }
+      commentCount: 0,
+    };
 
-    res(posts[post.id])
-  })
+    res(posts[post.id]);
+  });
 }
 
-function vote (token, id, option) {
+function vote(token, id, option) {
   return new Promise((res) => {
-    let posts = getData(token)
-    post = posts[id]
-    switch(option) {
-        case "upVote":
-            post.voteScore = post.voteScore + 1
-            break
-        case "downVote":
-            post.voteScore = post.voteScore - 1
-            break
-        default:
-            console.log(`posts.vote received incorrect parameter: ${option}`)
+    const posts = getData(token);
+    post = posts[id];
+    switch (option) {
+      case 'upVote':
+        post.voteScore += 1;
+        break;
+      case 'downVote':
+        post.voteScore -= 1;
+        break;
+      default:
+        console.log(`posts.vote received incorrect parameter: ${option}`);
     }
-    res(post)
-  })
+    res(post);
+  });
 }
 
-function disable (token, id) {
-    return new Promise((res) => {
-      let posts = getData(token)
-      posts[id].deleted = true
-      res(posts[id])
-    })
+function disable(token, id) {
+  return new Promise((res) => {
+    const posts = getData(token);
+    posts[id].deleted = true;
+    res(posts[id]);
+  });
 }
 
-function edit (token, id, post) {
-    return new Promise((res) => {
-        let posts = getData(token)
-        for (prop in post) {
-            posts[id][prop] = post[prop]
-        }
-        res(posts[id])
-    })
+function edit(token, id, post) {
+  return new Promise((res) => {
+    const posts = getData(token);
+    for (prop in post) {
+      posts[id][prop] = post[prop];
+    }
+    res(posts[id]);
+  });
 }
 
 function incrementCommentCounter(token, id, count) {
-  const data = getData(token)
+  const data = getData(token);
   if (data[id]) {
-    data[id].commentCount += count
+    data[id].commentCount += count;
   }
 }
 
@@ -136,5 +136,5 @@ module.exports = {
   disable,
   edit,
   getAll,
-  incrementCommentCounter
-}
+  incrementCommentCounter,
+};
