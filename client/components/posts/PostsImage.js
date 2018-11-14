1afa193f31daa41edb5133af7c923e5e868ae69e
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-14 15:41:17 -0200

    styles: fix post image text on mobile

--- client/components/posts/PostsImage.js
+++ client/components/posts/PostsImage.js
@@ -3,12 +3,14 @@ import React from 'react';
 const PostsImage = () => (
   <div className="posts-image">
     <div className="overlay-image">
-      <span className="title is-size-4-mobile is-size-1 posts-image--title">
-        OMAIA BLOG
-      </span>
-      <span className="title is-size-7-mobile is-size-4 posts-image--text">
-        Bringing information and knowledge for you
-      </span>
+      <div className="overlay-image--text">
+        <span className="title is-size-4-mobile is-size-1 posts-image--title">
+          OMAIA BLOG
+        </span>
+        <span className="title is-size-7-mobile is-size-4 posts-image--text">
+          Bringing information and knowledge for you
+        </span>
+      </div>
     </div>
   </div>
 );
