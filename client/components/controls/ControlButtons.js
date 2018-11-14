613afe07b17f2e3fa3d5441d904ef6d67980e574
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-13 23:11:41 -0200

    refactor: refactor folder for control buttons styles: change layout on mobile for controls

---
+++ client/components/controls/ControlButtons.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faThLarge } from '@fortawesome/free-solid-svg-icons';
+
+import { Select } from 'antd';
+const { Option } = Select;
+
+const ControlButtons = (props) => {
+  const {
+    sortBy, handleSort, onClick, setBoxLayout, setNormalLayout, isBoxLayout,
+  } = props;
+
+  const filterByType = (type) => {
+    const { order } = sortBy;
+    handleSort({
+      type,
+      order,
+    });
+  };
+
+  const filterByOrder = (order) => {
+    const { type } = sortBy;
+    handleSort({
+      type,
+      order,
+    });
+  };
+
+  return (
+    <div className="column is-12">
+      <div className="columns is-centered">
+        <div className="column is-8">
+          <div className="columns is-multiline is-flex">
+            <div className="column is-12-mobile is-9">
+              <div className="columns is-flex">
+                <div className="column is-8-mobile is-6 control-filters__container">
+                  <span className="control-filters--label">Filter by:</span>
+                  <div className="control-filters--buttons">
+                    <Select defaultValue={sortBy.type} onChange={filterByType}>
+                      <Option value="timestamp">Date</Option>
+                      <Option value="title">Title</Option>
+                      <Option value="author">Author</Option>
+                      <Option value="voteScore">Votes</Option>
+                      <Option value="commentCount">Comments</Option>
+                    </Select>
+                    <Select defaultValue={sortBy.order} onChange={filterByOrder}>
+                      <Option value="decrescent">Decrescent</Option>
+                      <Option value="crescent">Crescent</Option>
+                    </Select>
+                  </div>
+                </div>
+                <div className="column is-4-mobile is-3 control-filters__container">
+                  <span className="control-filters--label">Layout:</span>
+                  <div className="control-filters--layout">
+                    <a
+                      onClick={setNormalLayout}
+                      className={`control-filters--layout-button${!isBoxLayout ? '-active' : ''}`}
+                    >
+                      <FontAwesomeIcon icon={faBars} />
+                    </a>
+                    <a
+                      onClick={setBoxLayout}
+                      className={`control-filters--layout-button${isBoxLayout ? '-active' : ''}`}
+                    >
+                      <FontAwesomeIcon icon={faThLarge} />
+                    </a>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <div className="column is-12-mobile is-3 control-new-post">
+              <a
+                role="button"
+                className="button is-link is-pulled-right control-new-post-button"
+                onClick={onClick}
+              >
+              New Post
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ControlButtons.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default ControlButtons;
--- client/containers/Posts.js
+++ client/containers/Posts.js
@@ -9,7 +9,7 @@ import { postsValuesSelector } from 'Selectors/posts'; // eslint-disable-line
 import Header from 'Components/header/Header';
 import Post from 'Components/post/Post';
 import NewPost from 'Components/post/NewPost';
-import ControlButtons from 'Components/posts/ControlButtons';
+import ControlButtons from 'Components/controls/ControlButtons';
 import PostsImage from 'Components/posts/PostsImage';
 import PostNotFound from 'Components/posts/PostNotFound';
 
