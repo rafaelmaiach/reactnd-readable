45ca713e095b3e55a243fb49cefd55a090d12b7b
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-14 15:36:52 -0200

    feat: create footer

--- client/components/footer/Footer.js
+++ client/components/footer/Footer.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
+import { Icon } from 'antd';
+
 const Footer = () => (
   <footer className="footer">
-    <div className="content has-text-centered">
+    <div className="columns is-multiline footer__container">
+      <div className="column is-12 has-text-centered footer__container--top">
+        <span className="footer-text">OMAIA BLOG</span>
+        <span className="footer-text">by</span>
+        <span className="footer-text">
+          <a href="https://www.linkedin.com/in/rafaelmaiach/" target="_blank" rel="noopener noreferrer">
+            Rafael Maia Chieregatto
+          </a>
+        </span>
+      </div>
+      <div className="column is-12 has-text-centered footer__container--bottom">
+        <span className="footer-text">FOLLOW ME: </span>
+        <a href="http://github.com/rafaelmaiach" target="_blank" rel="noopener noreferrer">
+          <Icon type="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/rafaelmaiach/" target="_blank" rel="noopener noreferrer">
+          <Icon type="linkedin" />
+        </a>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
+
+/**
+ * <div className="content has-text-centered">
       <p>
         <strong>Bulma</strong>
       by
@@ -11,7 +39,4 @@ const Footer = () => (
       is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
       </p>
     </div>
-  </footer>
-);
-
-export default Footer;
+ */
