4523152930320973b39452e2876145f8588cc14e
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-11-16 23:13:31 -0200

    docs: Comment AppWrapper and remove unused functions

--- client/containers/AppWrapper.js
+++ client/containers/AppWrapper.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 
 import Footer from 'Components/footer/Footer';
 import Header from 'Components/header/Header';
@@ -6,16 +6,19 @@ import NewPost from 'Components/post/NewPost';
 import PostsImage from 'Components/posts/PostsImage';
 import Posts from './Posts';
 
-
-class AppWrapper extends Component {
+/**
+ * @class AppWrapper
+ * @description It's a wrapper to application to centralize the components
+ */
+class AppWrapper extends PureComponent {
   state = {
     isPostFormOpen: false,
   }
 
-  openPost = () => this.setState(() => ({ isPostFormOpen: true }));
-
-  closeForm = () => this.setState(() => ({ isPostFormOpen: false }));
-
+  /**
+   * @method AppWrapper#openPost
+   * @description Open the new post form to create a post
+   */
   toggleNewPost = () => this.setState(prevState => ({ isPostFormOpen: !prevState.isPostFormOpen }));
 
   render() {
@@ -25,8 +28,8 @@ class AppWrapper extends Component {
       <Fragment>
         <Header {...this.props} />
         <PostsImage />
-        <NewPost isPostFormOpen={isPostFormOpen} closeForm={this.closeForm} />
-        <Posts {...this.props} openPost={this.openPost} />
+        <NewPost isPostFormOpen={isPostFormOpen} closeForm={this.toggleNewPost} />
+        <Posts {...this.props} openPost={this.toggleNewPost} />
         <Footer />
       </Fragment>
     );
